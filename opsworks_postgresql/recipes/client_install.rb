84f452b84419e149003a9605d1e339709dc05ec6
Nino Milenović <nino.mil@gmail.com>
2015-12-10 13:06:50 +0100

    Add postgresql repo for v9.4

--- opsworks_postgresql/recipes/client_install.rb
+++ opsworks_postgresql/recipes/client_install.rb
@@ -1,3 +1,10 @@
+apt_repository "ubuntu-14.04-trusty-postgresql-9.4" do
+  uri "http://apt.postgresql.org/pub/repos/apt/"
+  distribution "trusty-pgdg"
+  components ["main"]
+  key "https://www.postgresql.org/media/keys/ACCC4CF8.asc"
+end
+
 package "postgresql-devel" do
   package_name value_for_platform(
     ["centos","redhat","fedora","amazon"] => {"default" => "postgresql-devel"},
@@ -9,7 +16,7 @@ end
 package "postgresql-client" do
   package_name value_for_platform(
     ["centos","redhat","fedora","amazon"] => {"default" => "postgresql"},
-    "default" => "postgresql-client"
+    "default" => "postgresql-client-9.4"
   )
   action :install
 end
